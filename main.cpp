915e79cd8502396a7809948b52277dfd1f6d34bc
jose <castrooj02@gmail.com>
2025-04-16 12:56:03 -0600

    delete agregados

--- main.cpp
+++ main.cpp
@@ -6,15 +6,34 @@ int main() {
     LevelFactory* factorySMW = new SuperMarioWorldFactory();
     LevelFactory* factorySMB3 = new SuperMarioBros3Factory();
 
-    cout << endl << "Elementos de Super Mario World:" << endl;
-    cout << factorySMW->createCoin()->belongsTo() << endl;
-    cout << factorySMW->createBlock()->belongsTo() << endl;
-    cout << factorySMW->createTurtle()->belongsTo() << endl << endl;
+    // objetos de SuperMarioWorld
+    Coin* SMWCoin = factorySMW->createCoin();
+    Block* SMWBlock = factorySMW->createBlock();
+    Turtle* SMWTurtle = factorySMW->createTurtle();
 
-    cout << "Elementos de Super Mario Bros 3:" << endl;
-    cout << factorySMB3->createCoin()->belongsTo() << endl;
-    cout << factorySMB3->createBlock()->belongsTo() << endl;
-    cout << factorySMB3->createTurtle()->belongsTo() << endl;
+    // objetos de SuperMarioBros3
+    Coin* SMB3Coin = factorySMB3->createCoin();
+    Block* SMB3Block = factorySMB3->createBlock();
+    Turtle* SMB3Turtle = factorySMB3->createTurtle();
+
+    cout << endl << "Elementos de Super Mario World:" << endl;
+    cout << SMWCoin->belongsTo() << endl;
+    cout << SMWBlock->belongsTo() << endl;
+    cout << SMWTurtle->belongsTo() << endl;
+
+    cout << endl << "Elementos de Super Mario Bros 3:" << endl;
+    cout << SMB3Coin->belongsTo() << endl;
+    cout << SMB3Block->belongsTo() << endl;
+    cout << SMB3Turtle->belongsTo() << endl;
+
+    delete factorySMW;
+    delete factorySMB3;
+    delete SMWCoin;
+    delete SMWBlock;
+    delete SMWTurtle;
+    delete SMB3Coin;
+    delete SMB3Block;
+    delete SMB3Turtle;
 
     return 0;
 }
